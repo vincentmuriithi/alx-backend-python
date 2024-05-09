d506b6d34fd8946c9cb154ff3201c2bfc79614f8
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-09 03:24:07 -0700

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
-
-import asyncio
+"""Execute multiple coroutines at the same time with async"""
 from typing import List
-
-
-task_wait_random = __import__("3-tasks").task_wait_random
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order
-    without using sort() because of concurrency.
-    """
-    wait_times = await asyncio.gather(
-            *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-            )
-    return sorted(wait_times)
+    """Spawns wait_random n times with the specified max_delay
+        and returns the list of all the delays (float values)."""
+    futures = [task_wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays

7dc538d9ed69503355967e4888b8a735b3a98f14
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-09 03:01:47 -0700

    removing trailng whitespaces

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order 
+    The list of the delays should be in ascending order
     without using sort() because of concurrency.
     """
     wait_times = await asyncio.gather(
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ task_wait_random = __import__("3-tasks").task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order 
+    The list of the delays should be in ascending order
     without using sort() because of concurrency.
     """
     wait_times = await asyncio.gather(
